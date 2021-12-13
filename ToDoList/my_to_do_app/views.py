c2e1710d994d5fb267ea56ec67800d1d832079b7
qwert <moonjy061130@gmail.com>
2021-12-13 04:52:18 +0000

    211213  - 1

--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'my_to_do_app/index.html')
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    return HttpResponse("create Todo를 할거야!=>"+user_input_str)
